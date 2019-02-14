02ecc3d2c1f9be0dd78319dfa4d6ad7f5b4a5ebb
keidongwon <keidongwon@gmail.com>
2019-02-14 18:22:03 +0900

    datetime 에 timnezone 설정

--- pybolt/util/datetime.py
+++ pybolt/util/datetime.py
@@ -1,8 +1,8 @@
 from datetime import timezone, datetime
 
 
-def utc_to_local(utc_dt):
+def utc_to_local(utc_dt, localtz=None):
 	if type(utc_dt) == str:
 		utc_dt = datetime.strptime(utc_dt, '%Y-%m-%d %H:%M:%S')
-	local_dt = utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+	local_dt = utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=localtz)
 	return local_dt.strftime('%Y-%m-%d %H:%M:%S')
