5745a00af329098dba9eb7bac75f19ac6924bbe0
keidongwon <keidongwon@gmail.com>
2019-02-19 13:49:55 +0900

    utc_to_local 업데이트 (pytz 사용)

--- pybolt/util/datetime.py
+++ pybolt/util/datetime.py
@@ -1,8 +1,9 @@
 from datetime import timezone, datetime
+import pytz
 
 
-def utc_to_local(utc_dt, localtz=None):
+def utc_to_local(utc_dt, local_tz=None):
 	if type(utc_dt) == str:
 		utc_dt = datetime.strptime(utc_dt, '%Y-%m-%d %H:%M:%S')
-	local_dt = utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=localtz)
+	local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
 	return local_dt.strftime('%Y-%m-%d %H:%M:%S')
